9c62b661dfd1052ac59fd0e6e4db69efb9e9e8a0
Carson <carsonf92@gmail.com>
2022-03-29 20:34:17 -0500

    build

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
+// Selectors
+
+const overlay = document.querySelector('.overlay');
 const grid = document.querySelector('.grid');
 const toggles = document.querySelectorAll('.switch');
-let solution = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let startState = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+const menu = document.querySelector('#menu');
+const instructions = document.querySelector('#instructions');
+const dismiss = document.querySelector('.modal__dismiss');
+const win = document.querySelector('#win');
+const restart = document.querySelector('#restart');
+
+// Global variables
+
+let solution = [];
+let startState = [];
+
+// Functions
 
 function toggleSwitches(event) {
     if (event.target.classList.contains('switch')) {
@@ -24,12 +37,17 @@ function checkStatus() {
 	});
 	
 	if (status) {
-		// win state
+		showWin();
 	}
 }
 
 function randomSolution() {
 	for (let i = 0; i < 25; i++) {
+		// reset arrays
+		solution[i] = 0;
+		startState[i] = 1;
+
+		// random input
 		solution[i] = Math.round(Math.random());
 	}
 }
@@ -59,28 +77,37 @@ function initializeGame() {
 }
 
 function showInstructions() {
-
+	overlay.classList.add('overlay--active');
+	instructions.classList.add('modal--active');
 }
 
 function dimissInstructions() {
-
+	overlay.classList.remove('overlay--active');
+	instructions.classList.remove('modal--active');
 }
 
 function showWin() {
-
+	overlay.classList.add('overlay--active');
+	win.classList.add('modal--active');
 }
 
 function newGame() {
-	
+	overlay.classList.remove('overlay--active');
+	win.classList.remove('modal--active');
+
+	randomSolution();
+	generateStartState();
+	initializeGame();
 }
 
 // Interactions
 
+menu.addEventListener('click', showInstructions);
+dismiss.addEventListener('click', dimissInstructions);
 grid.addEventListener('click', toggleSwitches);
 grid.addEventListener('click', checkStatus);
+restart.addEventListener('click', newGame);
 
 // Init Game
 
-randomSolution();
-generateStartState();
-initializeGame();
+newGame();
